92edd2c6e19aa180f29e7df8a6a21e44b87fdc93
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-25 09:46:41 +0200

    Added proper navigation to all pages

--- ttfonte-next/components/Navbar.js
+++ ttfonte-next/components/Navbar.js
@@ -15,24 +15,24 @@ export default function Navbar ({}) {
             &nbsp;Machines!
             <div className={styles.dropdown}>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/machines/ice-cream" className={styles.navLink}>
                   Ice cream machines
                 </Link>
               </div>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/machines/chocolate" className={styles.navLink}>
                   Chocolate machines
                 </Link>
               </div>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/machines/trucks" className={styles.navLink}>
                   Refrigerated trucks
                 </Link>
               </div>
             </div>
           </li>
           <li className={styles.navItem}>
-            <Link href="/" className={styles.navLink}>
+            <Link href="/contact" className={styles.navLink}>
               Contact us!
             </Link>
           </li>
@@ -55,22 +55,22 @@ export default function Navbar ({}) {
             &nbsp;Partners!
             <div className={styles.dropdown}>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/teknoice" className={styles.navLink}>
                   Teknoice
                 </Link>
               </div>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/packint" className={styles.navLink}>
                   Packint
                 </Link>
               </div>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/laief" className={styles.navLink}>
                   Laief
                 </Link>
               </div>
               <div className={styles.dropDiv}>
-                <Link href="/" className={styles.navLink}>
+                <Link href="/cold-car" className={styles.navLink}>
                   Cold car
                 </Link>
               </div>
