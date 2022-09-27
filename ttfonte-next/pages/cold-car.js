d5137c10b047519d996f728a7219fd1d6c07901d
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-27 18:15:06 +0200

    Added placeholder for cold car page

--- ttfonte-next/pages/cold-car.js
+++ ttfonte-next/pages/cold-car.js
@@ -1,5 +1,18 @@
 import Layout from "../components/Layout";
 
 export default function IceCream({}) {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <h1
+        style={{
+          height: "92vh",
+          fontSize: "4em",
+          textAlign: "center",
+          lineHeight: "92vh",
+        }}
+      >
+        This section isn&apos;t finished yet...
+      </h1>
+    </Layout>
+  );
 }
