17f39e5484c0b4cfb1a189262916b31a20be0e42
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-27 20:42:50 +0200

    implemented recaptcha

--- ttfonte-next/pages/_app.js
+++ ttfonte-next/pages/_app.js
@@ -2,9 +2,22 @@ import '../styles/globals.css'
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6LcTojYiAAAAAIA2sOYLZI32ayTG1ndtvVseMTXO"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
+  );
 }
 
 export default MyApp
