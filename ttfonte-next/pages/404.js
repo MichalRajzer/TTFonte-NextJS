751875ee14ee77ed15ba0f1a5193c5dfaaca0226
MichalRajzer <michal.rajzer03@gmail.com>
2022-09-27 18:32:17 +0200

    created custom error pages with auto redirect to home

---
+++ ttfonte-next/pages/404.js
@@ -0,0 +1,33 @@
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export default function fourOhFour({}) {
+  const [redirectSeconds, setRedirectSeconds] = useState(5);
+  const router = useRouter();
+  useEffect(() => {
+    if (redirectSeconds == 0) {
+      router.push('/');
+      return;
+    }
+
+    setTimeout(() => {
+      console.log(redirectSeconds);
+      setRedirectSeconds((redirectSeconds) => redirectSeconds - 1);
+    }, 1000);
+  }, [redirectSeconds]);
+  return (
+    <Layout>
+      <h1
+        style={{
+          height: "92vh",
+          fontSize: "4em",
+          textAlign: "center",
+          lineHeight: "92vh",
+        }}
+      >
+        This site doesn&apos;t exist. Redirecting in {redirectSeconds}!
+      </h1>
+    </Layout>
+  );
+}
