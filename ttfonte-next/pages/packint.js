157a678834729c4ac860911201615c5d5507d48b
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-26 00:46:04 +0100

    Moar visual changes

--- ttfonte-next/pages/packint.js
+++ ttfonte-next/pages/packint.js
@@ -6,27 +6,37 @@ import Link from "next/link";
 export default function Packint({}) {
   return (
     <Layout title="Packint s.r.l.">
-      <div className={styles.stdPadding}>
-        <h1 className={styles.header}>
-          <Link href="https://www.laief.it/?lang=en"> Packint s.r.l.</Link>
-        </h1>
-        <p className={styles.desc}>
-          Packint s.r.l. has been operating in the chocolate machinery sector
-          since 1992, directly or through other brands worldwide. Over these
-          years Packint has earned the trust of many companies ranging from
-          small family businesses to international chocolate manufacturers
-          including bean to bar producers. Their headquarters are located in the
-          north of Italy, 40 km south of Milan, where you can visit a complete
-          pilot plant from cacao beans to chocolate bars and chips for tests and
-          training.
-        </p>
-        <div className={styles.imgWrapper}>
-          <Image
-            src={"/imgs/packint/packint-chocolate-machines-logo.png"}
-            layout="fill"
-            objectFit="contain"
-            alt="The logo of LAIeF"
-          />
+      {" "}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "calc(100vh - 8vh)",
+        }}
+      >
+        <div className={styles.stdPadding}>
+          <h1 className={styles.header}>
+            <Link href="https://www.laief.it/?lang=en"> Packint s.r.l.</Link>
+          </h1>
+          <p className={styles.desc}>
+            Packint s.r.l. has been operating in the chocolate machinery sector
+            since 1992, directly or through other brands worldwide. Over these
+            years Packint has earned the trust of many companies ranging from
+            small family businesses to international chocolate manufacturers
+            including bean to bar producers. Their headquarters are located in
+            the north of Italy, 40 km south of Milan, where you can visit a
+            complete pilot plant from cacao beans to chocolate bars and chips
+            for tests and training.
+          </p>
+          <div className={styles.imgWrapper}>
+            <Image
+              src={"/imgs/packint/packint-chocolate-machines-logo.png"}
+              layout="fill"
+              objectFit="contain"
+              alt="The logo of LAIeF"
+            />
+          </div>
         </div>
       </div>
     </Layout>
--- ttfonte-next/pages/teknoice.js
+++ ttfonte-next/pages/teknoice.js
@@ -6,26 +6,36 @@ import Link from "next/link";
 export default function Teknoice({}) {
   return (
     <Layout title="Teknoice S.r.l.">
-      <div className={styles.stdPadding}>
-        <h1 className={styles.header}>
-          <Link href="https://www.teknoice.com/"> Teknoice S.r.l. </Link>
-        </h1>
-        <p className={styles.desc}>
-          TEKNOICE is one of the largest Italian manufacturers of industrial and
-          semi-industrial ice cream machines. They have over 35 years&apos;
-          experience in the ice cream sector and know-how, creativity and
-          technicalinnovation research. Moreover, the great desire to improve
-          the product offered has enabled them to grow rapidly and conquer the
-          global market in just a few years by manufacturing complete lines:
-          from pasteurisation to filling, extrusion and hardening tunnels.
-        </p>
-        <div className={styles.imgWrapper}>
-          <Image
-            src={"/imgs/teknoice/TeknoICE Logo.png"}
-            layout="fill"
-            objectFit="contain"
-            alt="The logo of LAIeF"
-          />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "calc(100vh - 8vh)",
+        }}
+      >
+        <div className={styles.stdPadding}>
+          <h1 className={styles.header}>
+            <Link href="https://www.teknoice.com/"> Teknoice S.r.l. </Link>
+          </h1>
+          <p className={styles.desc}>
+            TEKNOICE is one of the largest Italian manufacturers of industrial
+            and semi-industrial ice cream machines. They have over 35
+            years&apos; experience in the ice cream sector and know-how,
+            creativity and technical innovation research. Moreover, the great
+            desire to improve the product offered has enabled them to grow
+            rapidly and conquer the global market in just a few years by
+            manufacturing complete lines: from pasteurisation to filling,
+            extrusion and hardening tunnels.
+          </p>
+          <div className={styles.imgWrapper}>
+            <Image
+              src={"/imgs/teknoice/TeknoICE Logo.png"}
+              layout="fill"
+              objectFit="contain"
+              alt="The logo of LAIeF"
+            />
+          </div>
         </div>
       </div>
     </Layout>
