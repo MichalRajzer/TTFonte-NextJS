f596a493576084dda3e0234635ddd4c76fb38dee
Michał Rajzer <michal.rajzer03@gmail.com>
2023-04-16 20:20:19 +0200

    Added language switcher

--- ttfonte-next/components/Layout.js
+++ ttfonte-next/components/Layout.js
@@ -2,6 +2,55 @@ import Navbar from "./Navbar";
 import Head from "next/head";
 import styles from "../styles/Layout.module.css";
 import ChristmasPupup from "./ChristmasPupup";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const LanguageSwitcher = ({ lang }) => {
+  const router = useRouter();
+  console.log(lang);
+
+  let paths = {
+    "/": "/pl",
+    "/pl": "/",
+    "/pl/kontakt": "/contact",
+    "/contact": "/pl/kontakt",
+    "/pl/maszyny/lody": "/machines/ice-cream",
+    "/machines/ice-cream": "/pl/maszyny/lody",
+    "/pl/maszyny/czekolada": "/machines/chocolate",
+    "/machines/chocolate": "/pl/maszyny/czekolada",
+    "/pl/maszyny/ciezarowki": "/machines/trucks",
+    "/machines/trucks": "/pl/maszyny/ciezarowki",
+    "/pl/laief": "/laief",
+    "/laief": "/pl/laief",
+    "/pl/teknoice": "/teknoice",
+    "/teknoice": "/pl/teknoice",
+    "/pl/packint": "/packint",
+    "/packint": "/pl/packint",
+    "/pl/coldcar": "/coldcar",
+    "/coldcar": "/pl/coldcar",
+  };
+
+  let page = router.asPath.split("#")[0];
+
+  return (
+    <div className={styles.LangSwitch}>
+      <h6>
+        {lang === "pl" ? (
+          <Link href={paths[page]}>Pl</Link>
+        ) : (
+          <Link href={paths[page]}>En</Link>
+        )}
+      </h6>
+      <span>
+        {lang === "pl" ? (
+          <Link href={paths[page]}>Kliknij tu aby zmienić język</Link>
+        ) : (
+          <Link href={paths[page]}>Click here to change the language</Link>
+        )}
+      </span>
+    </div>
+  );
+};
 
 export default function Layout({ title, children, lang, description }) {
   return (
@@ -29,6 +78,7 @@ export default function Layout({ title, children, lang, description }) {
       </Head>
       {/* <ChristmasPupup /> */}
       <Navbar lang={lang} />
+      <LanguageSwitcher lang={lang} />
       <div className={styles.content}>{children}</div>
     </div>
   );
